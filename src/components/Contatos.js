13178bbc9c912ee18e47f7947c6cece3043c5272
levymc <levymcruz@gmail.com>
2023-05-03 20:03:05 -0300

    trocando o nome do usuário

--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -1,3 +1,4 @@
+import lapis from "../img/lapis.svg"
 import ruivo from "../img/ruivo.png"
 import kevin from "../img/kevin.PNG"
 
@@ -16,10 +17,10 @@ export default function Contatos(){
             nome: "kevin",
             image: kevin
         },
-    ]
+    ];
     return (
         <div className="contatos">
-            <div className="user flex"><img id="userImg" src={ruivo} alt="Eu" /><h4>levymc</h4></div>
+            <div className="user flex"><img id="userImg" src={ruivo} alt="Eu" /><h4 id="usuario">levymc</h4><img onClick={promptName} id="lapis" src={lapis} alt="Lapis" /></div>
             <div className="conteudo-contatos">
                 <div className='topoContatos'><h4>Segestões para você</h4> <h5>Ver tudo</h5></div>
                 {contatos.map((user) => 
@@ -37,4 +38,12 @@ export default function Contatos(){
             </div>
         </div>
     )
+}
+
+function promptName (){
+    let usuario = document.getElementById("usuario");
+    let newName = prompt("Digite o novo nome de usuário");
+    usuario.innerHTML = '';
+    usuario.innerHTML += newName
+    document.querySelector("#lapis").removeEventListener("click", promptName);
 }
