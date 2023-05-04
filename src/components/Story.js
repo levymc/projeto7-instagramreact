96993a4387bea28f7ea847ef3c42f71d69de1096
levymc <levymcruz@gmail.com>
2023-05-03 21:07:26 -0300

    finalizando a formatação dos stories

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,8 +1,25 @@
+import thais from "../img/thais.png"
 
 export default function Story(){
+    const dados = [{
+        nome: "thais",
+        image: thais
+    }]
+
     return (
-        <div className="story flex">
-            Oi
-        </div>
+        <>
+        
+            {dados.map((dado) => 
+                <div className="container-story">
+                    <div className="story flex">
+                        <img src={dado.image} alt={dado.nome} />
+                    </div>
+                    <h3>{dado.nome}</h3>
+                </div>
+                
+                
+                )}
+        
+        </>
     )
 }
