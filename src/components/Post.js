ad06117ffbd5ebb41033920020fde797584117c2
levymc <levymcruz@gmail.com>
2023-05-04 19:12:27 -0300

    adicionando o icone de marcador e finalizando o layout

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,7 @@ import ruivo from "../img/ruivo.png"
 import chatbubble from "../img/chatbubble.svg"
 import heart from "../img/heart.svg"
 import aviao from "../img/aviao.svg"
+import marcador from "../img/marcador.svg"
 
 export default function Post(){
     const dados = [
@@ -10,6 +11,7 @@ export default function Post(){
             userName: "levymc", 
             urlImage: ruivo,
             quemCurtiu: "frederico",
+            imgQuemCurtiu: ruivo,
             qntsPessoas: 199823
         }
     ]
@@ -26,9 +28,10 @@ export default function Post(){
                         <img className="iconJump" src={heart} alt="Heart" />
                         <img className="iconJump" src={chatbubble} alt="Chatbubble" />
                         <img className="iconJump" src={aviao} alt="Airplane" />
+                        <img className="iconJump marcador" src={marcador} alt="Marcador" />
                     </div>
                     <div className="legenda flex">
-                        img <h3>Curtido por {dado.quemCurtiu} e outras {dado.qntsPessoas} pessoas</h3>
+                        <img src={dado.imgQuemCurtiu} className="imageQuemCurtiu" alt="" /> <h3>Curtido por {dado.quemCurtiu} e outras {dado.qntsPessoas} pessoas</h3>
                     </div>
                     
                 </div>
